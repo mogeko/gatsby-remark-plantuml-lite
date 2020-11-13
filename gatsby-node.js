0d7d04d82d4a7bd906a0e9292943d0aa26e9de4f
Mogeko <zhengjunyi@live.com>
2020-11-13 22:05:36 +0100

    feat(Configuration): Define an options schema

---
+++ gatsby-node.js
@@ -0,0 +1,7 @@
+exports.pluginOptionsSchema = ({ Joi }) => {
+  return Joi.object({
+    imageType: Joi.string()
+      .default("svg")
+      .description("Type of PlantUML image returned."),
+  })
+}
