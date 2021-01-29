fb0bc7d9b615452ac72ec3ad112f0d73745d80d2
Mogeko <zhengjunyi@live.com>
2021-01-29 22:02:31 +0100

    feat(PlantUML): Support custom PlantUML server url
    issues #13

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,9 @@ exports.pluginOptionsSchema = ({ Joi }) => {
   return Joi.object({
     imageType: Joi.string()
       .default("svg")
-      .description("Type of PlantUML image returned from Web Server"),
+      .description("Type of PlantUML image returned from Web Server."),
+    server: Joi.string()
+      .default("https://www.plantuml.com/plantuml")
+      .description("PlantUML server to generate UML diagrams on-the-fly."),
   })
 }
--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,18 @@ interface ParsedTypes {
 
 interface OptionTypes {
   imageType?: "svg" | "png",
+  server?: string,
 }
 
 export default function remarkPlantUML({ markdownAST }: ParsedTypes, pluginOptions?: OptionTypes) {
   const imageType = pluginOptions?.imageType
     ? pluginOptions.imageType
     : "svg"
+  const server = pluginOptions?.server
+    ? pluginOptions.server
+    : "https://www.plantuml.com/plantuml"
   return nodeOperator(markdownAST, (encoded) => {
-    return `https://www.plantuml.com/plantuml/${imageType}/${encoded}`
+    return `${server}/${imageType}/${encoded}`
   })
 }
 
