06836da3379f592507cf98db8f0aba7aa73007d3
Mogeko <zhengjunyi@live.com>
2021-11-08 15:40:19 +0000

    refactor: reconstruct lib.test.ts
    Issue #26

--- tests/lib.test.ts
+++ tests/lib.test.ts
@@ -1,25 +1,60 @@
 // @ts-nocheck
+import { Image, Paragraph, Root } from 'mdast';
 import remark = require('remark');
 import nodeOperator from '../src/lib';
 
-const raw = remark().parse(`
+let rawAst: Root;
+
+beforeAll(() => {
+  rawAst = remark().parse(`
   \`\`\`plantuml
   @startuml
   A -> B: Hello / 你好'
   @enduml
   \`\`\`
   `);
-
-test('基础测试', () => {
-  const example = require('./examples/test.data.0.json');
-  const result = nodeOperator(raw, (encoded) => {
-    return `https://www.plantuml.com/plantuml/svg/${encoded}`;
-  });
-  expect(result).toStrictEqual(example);
 });
 
-test('测试编码是否正确', () => {
-  nodeOperator(raw, (encoded) => {
+describe('基础测试', () => {
+  let resAst: Root;
+  let paragraph: Paragraph;
+  let image: Image;
+
+  beforeAll(() => {
+    resAst = nodeOperator(rawAst, (encoded) => {
+      return `https://www.plantuml.com/plantuml/svg/${encoded}`;
+    });
+    paragraph = resAst.children[0];
+    image = paragraph.children[0];
+  });
+
+  test('测试 Wrap (Paragraph) 的类型', () => {
+    expect(paragraph.type).toEqual('paragraph');
+  });
+
+  test('测试 Image 的类型', () => {
+    expect(image.type).toEqual('image');
+  });
+
+  test('测试 Image 的 Alt', () => {
+    expect(image.alt).toEqual('PlantUML');
+  });
+
+  test('测试 Image 的 Title', () => {
+    expect(image.title).toEqual(null);
+  });
+
+  test('测试 Image 的 Url', () => {
+    expect(image.url).toMatch(/https:\/\/www.plantuml.com/);
+    expect(image.url).toMatch(/svg/);
+    expect(image.url).toMatch(
+      /SoWkIImgAStDuN9KqBLJSB9Iy4ZDoSbNq5TuidV1qwLxrRaSKlDIWF80/
+    );
+  });
+});
+
+test('测试能否正确编码', () => {
+  nodeOperator(rawAst, (encoded) => {
     expect(encoded).toStrictEqual(
       'SoWkIImgAStDuN9KqBLJSB9Iy4ZDoSbNq5TuidV1qwLxrRaSKlDIWF80'
     );
