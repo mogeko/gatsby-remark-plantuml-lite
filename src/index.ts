9438a8f97ee766d288017904081674ce73093eca
Mogeko <zhengjunyi@live.com>
2024-09-28 23:05:20 +0800

    feat: create `mdast` by `unist-builder`

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-import type { Root, Paragraph } from "mdast";
-import { visit } from "unist-util-visit";
-import { paragraph, image } from "mdast-builder";
 import { encodeSync } from "@/encoder";
+import type { Root } from "mdast";
+import { u } from "unist-builder";
+import { visit } from "unist-util-visit";
 
 export default function remarkPlantUML(
-  { markdownAST }: ParsedTypes,
-  pluginOptions?: OptionTypes,
+  { markdownAST }: { markdownAST: Root },
+  pluginOptions?: {
+    imageType?: "svg" | "png"; // Type of PlantUML image returned from Web Server
+    server?: string; // PlantUML server to generate UML diagrams on-the-fly
+    codeBlockLang?: string; // Name of the codeblock languange
+    title?: string; // Specifies the title property of the generated PlantUML image
+    alt?: string; // Specifies the alt property of the generated PlantUML image
+  },
 ): Root {
   const imageType = pluginOptions?.imageType ?? "svg";
   const server = pluginOptions?.server
@@ -17,27 +23,15 @@ export default function remarkPlantUML(
 
   visit(markdownAST, "code", (node, index, parents) => {
     if (node.lang === codeBlockLang && parents) {
-      parents.children[index ?? 0] = paragraph(
-        image(
-          `${server}/${imageType}/${encodeSync(node.value)}`,
-          pluginOptions?.title,
-          pluginOptions?.alt ?? codeBlockLang,
-        ),
-      ) as Paragraph;
+      parents.children[index ?? 0] = u("paragraph", {}, [
+        u("image", {
+          url: `${server}/${imageType}/${encodeSync(node.value)}`,
+          title: pluginOptions?.title,
+          alt: pluginOptions?.alt ?? codeBlockLang,
+        }),
+      ]);
     }
   });
 
   return markdownAST;
 }
-
-type ParsedTypes = {
-  markdownAST: Root;
-};
-
-type OptionTypes = {
-  imageType?: "svg" | "png";
-  server?: string;
-  codeBlockLang?: string;
-  title?: string;
-  alt?: string;
-};
